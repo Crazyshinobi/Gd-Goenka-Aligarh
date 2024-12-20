d7353370850785228f573d98d9c44aaf726b4882
crazyshinobi <chetansingh410@gmail.com>
2024-12-20 16:21:09 +0530

    minor change

--- client/src/admin/Dashboard.jsx
+++ client/src/admin/Dashboard.jsx
@@ -84,7 +84,7 @@ export const Dashboard = () => {
   return (
     <>
       <AdminLayout />
-      <div className="lg:p-6 sm:ml-64 dark:bg-gray-800 h-screen">
+      <div className="lg:p-6 sm:ml-64 dark:bg-gray-800 min-h-screen">
         <div className="p-6 border-2 border-gray-200 rounded-lg dark:border-white mt-14 bg-white dark:bg-gray-700 shadow-lg">
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-4 lg:gap-6">
             {/* Card 1 */}
